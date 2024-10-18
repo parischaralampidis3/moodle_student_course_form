426e4e523798934837c431d9144f69826583d6eb
paris charalampidis <parou.char@gmail.com>
2024-10-18 12:18:45 +0300

    creating local plugin structure

---
+++ local/user_register/settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * You may have settings in your plugin
+ *
+ * @package    local_userRegister
+ * @copyright  2014 Surya Pratap
+ * @license    http://www.gnu.org/copyleft/gpl.html gnu gpl v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+if ($ADMIN->fulltree) {
+}

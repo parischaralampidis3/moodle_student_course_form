794e5dde7382b346380871943a7e00fec19f135e
paris charalampidis <parou.char@gmail.com>
2024-10-21 09:23:16 +0300

    working at email verification file

--- local/user_register/email_verification.php
+++ local/user_register/email_verification.php
@@ -1,5 +1,29 @@
 <?php
 
+require_once(__DIR__ ."/.././config.php");
 
+global $DB;
 
+if(isset($GET['token'])){
+    $token = $_GET['token'];
+
+$user = $DB->get_record('user_register',['verification_token'=>$token]);
+}
+
+if(!$user){
+    echo "Invalid or expired token";
+}
+
+$token_lifetime = 24*60*60;
+
+if(time() - $user->token_created_at  > $token_lifetime){
+    echo 'Token is expired';
+    exit;
+}
+
+$user->verified = 1;
+$user->verification_token = null;
+$DB->update_record('user_register',$user);
+
+echo "Your email has been verified! You can now log in!";
 ?>
